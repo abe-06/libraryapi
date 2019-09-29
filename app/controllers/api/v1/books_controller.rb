10a24e3327c170952c42344d74d931382e796b85
unknown <ibra.noguera@gmail.com>
2019-09-29 19:46:28 -0300

    Needs testing ad rate-limts

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,10 +1,63 @@
 module Api
-  module v1
+  module V1
     class BooksController < ApplicationController
+
       def index
         books = Book.all
-        render json: {status 'SUCCESS', message:'Loaded Books', data:books}, status: :ok
+        render json: {status: 'SUCCESS', message:'Loaded Books', data:books}, status: :ok
       end
+
+      def show
+        book = Book.find_by id: params[:id]
+        render json: {status: 'SUCCESS', message:'Loaded Book', data:book}, status: :ok
+      end
+
+      def search_collection
+        books = Book.where(["collection = ?", "#{params[:collection]}"])
+        render json: {status: 'SUCCESS', message:'Loaded Collection', data:books}, status: :ok
+      end
+
+      def search_title
+        books = Book.where("title like ?", "%#{params[:title]}%")
+        render json: {status: 'SUCCESS', message:'Loaded Books', data:books}, status: :ok
+      end
+
+      def search_isbn
+        books = Book.where("isbn like ?", "%#{params[:isbn]}%")
+        render json: {status: 'SUCCESS', message:'Loaded Books', data:books}, status: :ok
+      end
+
+      def create
+        book = Book.new(book_params)
+
+        if book.save
+          render json: {status: 'SUCCESS', message:'Book added to the database', data:book}, status: :ok
+        else
+          render json: {status: 'ERROR', message:'Book NOT added to the database', data:book.errors}, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        book = Book.find_by id: params[:id]
+        book.destroy
+        render json: {status: 'SUCCESS', message:'Book deleted from the database', data:book}, status: :ok
+      end
+
+      def update
+        book = Book.find_by id: params[:id]
+        if book.update_attributes(book_params)
+          render json: {status: 'SUCCESS', message:'Book information updated', data:book}, status: :ok
+        else
+          render json: {status: 'ERROR', message:'Book information NOT updated', data:book.errors}, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def book_params
+        params.permit(:title, :author, :genre, :price, :collection, :description, :isbn)
+      end
+
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :books
+      get 'books/collections/:collection', to: 'books#search_collection'
+      get 'books/titles/:title', to: 'books#search_title'
+      get 'books/isbn/:isbn', to: 'books#search_isbn'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
